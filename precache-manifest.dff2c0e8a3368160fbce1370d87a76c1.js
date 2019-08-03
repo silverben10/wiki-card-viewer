5e45f0bd38fc668a980a986c5bee2c5d92c6303b
Ben Silverman <bensilverman10@gmail.com>
2019-08-03 18:24:02 +0100

    Updates

---
+++ precache-manifest.dff2c0e8a3368160fbce1370d87a76c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ac9bcb5d20a7a427126feae39659d1",
+    "url": "/wiki-card-viewer/index.html"
+  },
+  {
+    "revision": "ce17c95862a296cab069",
+    "url": "/wiki-card-viewer/static/css/main.4f6709c6.chunk.css"
+  },
+  {
+    "revision": "62b5784608f3212890b6",
+    "url": "/wiki-card-viewer/static/js/2.11a6ef3a.chunk.js"
+  },
+  {
+    "revision": "ce17c95862a296cab069",
+    "url": "/wiki-card-viewer/static/js/main.cc2b333d.chunk.js"
+  },
+  {
+    "revision": "7e43910bc90c196e0e0a",
+    "url": "/wiki-card-viewer/static/js/runtime~main.99452bd2.js"
+  }
+]);
